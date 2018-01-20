f2f2f5e4d72fa40e13c24f0016f722e2374c52cf
Petter Remen <petter.remen@gmail.com>
2018-01-20 12:45:14 +0100

    refactor tests

--- src/test/java/com/meltwater/owl/hello/ApplicationTest.java
+++ src/test/java/com/meltwater/owl/hello/ApplicationTest.java
@@ -1,17 +1,16 @@
 package com.meltwater.owl.hello;
 
 import com.google.common.base.Charsets;
-import com.google.common.io.CharStreams;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,21 +33,46 @@ class ApplicationTest {
     @Test
     @DisplayName("The /health endpoint is successful")
     void the_health_endpoint_is_successful() throws Exception {
-        HttpResponse httpResponse = Request.Get(baseUrl + "/health")
-            .execute()
-            .returnResponse();
-        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
+        Response response = get("/health");
+        assertThat(response.statusCode).isEqualTo(200);
     }
 
     @Test
     @DisplayName("The / endpoint is successful")
     void the_root_endpoint_is_successful() throws Exception {
-        HttpResponse httpResponse = Request.Get(baseUrl + "/")
+        Response response = get("/");
+        assertThat(response.statusCode).isEqualTo(200);
+        assertThat(response.body).isEqualTo("Hello World!");
+    }
+
+    @Test
+    @DisplayName("The /metrics endpoint is successful")
+    void the_metrics_endpoint_is_successful() throws Exception {
+        Response response = get("/metrics");
+        assertThat(response.statusCode).isEqualTo(200);
+        assertThat(response.contentType).isEqualTo("text/plain; version=0.0.4; charset=utf-8");
+    }
+
+    private Response get(String path) throws IOException {
+        HttpResponse httpResponse = Request.Get(baseUrl + path)
             .execute()
             .returnResponse();
-        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
-        InputStream inputStream = httpResponse.getEntity().getContent();
-        String content = CharStreams.toString(new InputStreamReader(inputStream, Charsets.UTF_8));
-        assertThat(content).isEqualTo("Hello World!");
+        return new Response(
+            httpResponse.getStatusLine().getStatusCode(),
+            httpResponse.getFirstHeader("Content-Type").getValue(),
+            new String(EntityUtils.toByteArray(httpResponse.getEntity()), Charsets.UTF_8)
+        );
+    }
+
+    private static class Response {
+        final int statusCode;
+        final String contentType;
+        final String body;
+
+        Response(int statusCode, String contentType, String body) {
+            this.statusCode = statusCode;
+            this.contentType = contentType;
+            this.body = body;
+        }
     }
 }
